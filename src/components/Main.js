050351e9b5cbdd65e6fe34c37d125cd3b113fcc9
Rafael Graunke <rafael.graunke@yahoo.com>
2022-03-15 21:16:54 -0300

    Editando e removendo tarefas

--- src/components/Main.js
+++ src/components/Main.js
@@ -8,6 +8,7 @@ export default class Main extends Component {
   state = {
     novaTarefa: '',
     tarefas: [],
+    index: -1,
   };
 
   handleChange = (e) => {
@@ -16,19 +17,45 @@ export default class Main extends Component {
     });
   };
 
-  handleSubmit = (e) => {
-    e.preventDefault();
+  handleEdit = (e, index) => {
     const { tarefas } = this.state;
-    let { novaTarefa } = this.state;
-    novaTarefa = novaTarefa.trim();
+    this.setState({
+      index,
+      novaTarefa: tarefas[index],
+    });
+  };
 
-    if (tarefas.indexOf(novaTarefa) !== -1) return;
-
+  handleDelete = (e, index) => {
+    const { tarefas } = this.state;
     const novasTarefas = [...tarefas];
+    novasTarefas.splice(index, 1);
 
     this.setState({
-      tarefas: [...novasTarefas, novaTarefa],
+      tarefas: [...novasTarefas],
     });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const { tarefas, index } = this.state;
+    let { novaTarefa } = this.state;
+    novaTarefa = novaTarefa.trim();
+    const novasTarefas = [...tarefas];
+
+    if (index !== -1) {
+      novasTarefas[index] = novaTarefa;
+      this.setState({
+        tarefas: [...novasTarefas],
+        novaTarefa: '',
+        index: -1,
+      });
+    } else {
+      if (tarefas.indexOf(novaTarefa) !== -1) return;
+      this.setState({
+        tarefas: [...novasTarefas, novaTarefa],
+        novaTarefa: '',
+      });
+    }
   };
 
   render() {
@@ -44,12 +71,18 @@ export default class Main extends Component {
         </form>
 
         <ul className="tarefas">
-          {tarefas.map((tarefa) => (
+          {tarefas.map((tarefa, index) => (
             <li key={tarefa}>
               {tarefa}
               <span>
-                <FaEdit className="fa-edit" />
-                <FaWindowClose className="fa-remove" />
+                <FaEdit
+                  onClick={(e) => this.handleEdit(e, index)}
+                  className="fa-edit"
+                />
+                <FaWindowClose
+                  onClick={(e) => this.handleDelete(e, index)}
+                  className="fa-remove"
+                />
               </span>
             </li>
           ))}
